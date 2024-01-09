b305b5982f70e15f655e4571da74a51412a0e18e
Matteo Collina <hello@matteocollina.com>
2024-01-09 12:06:11 +0100

    init
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import fastify from 'fastify';
+import { Http2Server } from 'http2'
+
+
+const server = fastify();
+
+const plugin: FastifyPluginAsyncJsonSchemaToTs<{}, Http2Server> = async function (fastify, _opts) {
+  fastify.post(
+    '/',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          properties: {
+            x: { type: 'string' },
+            y: { type: 'number' },
+            z: { type: 'boolean' },
+          },
+          required: ['x', 'y', 'z'],
+        } as const,
+      },
+    },
+    (req) => {
+      /// The `x`, `y`, and `z` types are automatically inferred
+      const { x, y, z } = req.body;
+    }
+  );
+};
+
+server.register(plugin);
+
+server.listen({ port: 3000 });
